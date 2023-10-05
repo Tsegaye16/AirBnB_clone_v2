13d9b3923f24bd77e57b3b4e2191c97289c6a91d
Tsegaye16 <abewatsegaye16@gmail.com>
2023-10-05 00:42:02 -0700

    djskdsa

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,11 @@
+# Configures a web server for deployment of web_static.
+
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
