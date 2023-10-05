1ad2c4441863c33ec548ba7f7edb5b2020bf2f68
Tsegaye16 <abewatsegaye16@gmail.com>
2023-10-05 01:12:31 -0700

    dsakjd

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -9,3 +9,29 @@ $nginx_conf = "server {
 
     location /hbnb_static {
         alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
