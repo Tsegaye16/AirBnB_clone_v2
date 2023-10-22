1bf930301b04142b5574af2b8a8699add96b4ba7
Tsegaye16 <abewatsegaye16@gmail.com>
2023-10-22 13:07:02 -0700

    djskd

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,9 +21,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    '''display “C ” followed by the value of the text variable
-	(replace underscore _ symbols with a space )
-    '''
+    # display “C ” followed by the value of the text variable
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
